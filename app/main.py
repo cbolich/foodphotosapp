802fb977890abf273c7701f19d5a4f739ad9d830
jacob <jacobfrabutt@gmail.com>
2023-04-26 13:11:40 -0700

    Roy: can you help fix this?

--- app/main.py
+++ app/main.py
@@ -3,13 +3,19 @@ from pymongo import MongoClient
 from fastapi.encoders import jsonable_encoder
 from typing import List
 from models import Downloads, UserRequest
+import asyncio
+import json
+import requests
+import io
+import base64
+from PIL import Image, PngImagePlugin
 
 app = FastAPI()
 
 """SET UP / MISC """
 @app.on_event("startup")
 def startup_db_client():
-    app.mongodb_client = MongoClient("mongodb+srv://{USER}:{PASSWORD}@foodphotosapp.4vgexei.mongodb.net/?retryWrites=true&w=majority")
+    app.mongodb_client = MongoClient("mongodb+srv://jfrabut2:yCHemjm0O8FEbDt3@foodphotosapp.4vgexei.mongodb.net/?retryWrites=true&w=majority")
     app.database = app.mongodb_client["fpa"]
 
 @app.on_event("shutdown")
@@ -36,22 +42,42 @@ async def generate(request: Request, userRequest: UserRequest):
 
     # log the user request
     app.database.requests.insert_one(user_request_dict)
+    print("logged")
 
     # improve prompt
 
     # choose relevant pose
 
     # prepare settings
-    settings = app.database.settings.find_one({"settings_id": 1})
+    settings = app.database.settings.find_one({"settings_id": 0})
     if (not settings):
         return {"status": "error"}
 
-    # send to A111
 
+    print(settings)
+
+    # pass in actual prompt
+    settings["prompt"] = user_request_dict["prompt"]
+    settings["negative_prompt"] = "blurry, low quality, animated, cartoon, silverware, fork, knife, spoon, chopsticks,"
+    
+
+
+    # send to A111
+    asyncio.set_event_loop_policy(asyncio.DefaultEventLoopPolicy())
+
+    url = "http://192.168.1.158:7860/sdapi/v1/txt2img"
+    response = requests.post(url, json=payload)
+
+    r = response.json()
+
+    # receive generate images back and save 
+    for i in r['images']:
+        image = Image.open(io.BytesIO(base64.b64decode(i.split(",",1)[0])))
+        image.save(f'output{i}.png')
 
 
     # get response back
-    
+
 
     # save reponses to data base
     """ 
